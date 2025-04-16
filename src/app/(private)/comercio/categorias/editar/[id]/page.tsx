e058be1ff27474ce670de68612c69dfa6ee93979
kayrongradilone <kayrongradilone@hotmail.com>
2025-04-16 18:08:31 -0300

    Feat-AddingEditFormCompanyCategory

---
+++ src/app/(private)/comercio/categorias/editar/[id]/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import Header from "@/components/header";
+import { CompanyContext } from "@/providers/company";
+import FormUpdateCompany from "@/components/painel/cards/company/forms/update-company";
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+export default function EditCompanyPage({ params }: Props) {
+  const { SelfCompany, company } = useContext(CompanyContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    params.then(({ id }) => {
+      SelfCompany(id);
+      setLoading(false);
+    });
+  }, [params]);
+
+  if(!company?.id){
+    return <h1>ID não encontrado</h1>
+  }
+
+  return (
+    <div className="flex flex-col h-screen bg-primary-light overflow-hidden">
+      <Header title={`Edita Comercio - ${company?.name}`} buttonHidden={true} />
+      <div className="flex-1 overflow-hidden p-6">
+        {loading || !company ? (
+          <SkeletonTagForm />
+        ) : (
+          <FormUpdateCompany companyData={company} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function SkeletonTagForm() {
+  return (
+    <div className="animate-pulse space-y-4">
+      <div className="h-10 bg-gray-300 rounded"></div>
+      <div className="h-10 bg-gray-300 rounded"></div>
+      <div className="h-10 bg-gray-300 rounded"></div>
+    </div>
+  );
+}
